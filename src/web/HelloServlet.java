2955a65ac706a3048eee25a90c13cb69261854d5
taizlinger123 <837337164@qq.com>
2020-01-05 19:40:18 +0800

    2020-1-5

--- src/web/HelloServlet.java
+++ src/web/HelloServlet.java
@@ -10,11 +10,22 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet{
    public void service(HttpServletRequest request, HttpServletResponse response)throws ServletException,IOException{
+	   //读取请求参数值
+	   String qty = request.getParameter("qty");
+	   /*
+	    * 通常需要对请求参数值做一些检查，比如，检查qty是不是一个合法的数字。
+	    */
+	   //处理
+	   String greeting = "";
+	   for(int i=0;i<Integer.parseInt(qty);i++){
+		    greeting += "<h1>hello Kitty</h1>";
+	   }
 	   //设置服务器返回的数据类型	
 	   response.setContentType("text/html");
 	   PrintWriter out = response.getWriter();
-	   out.println("<h1 style='color:red;'>" +
-	   		"Hello Kitty</h1>");
+	   out.println(greeting);
 	   out.close();
    }
 }
+
+//http://localhost:8080/web01/hello?qty=8
