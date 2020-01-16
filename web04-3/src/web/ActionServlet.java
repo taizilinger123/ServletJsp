4f2d4ba99cfa6cc9b5b02bef9213583853e70cfb
taizlinger123 <837337164@qq.com>
2020-01-16 21:18:38 +0800

    2020-1-16

--- web04-3/src/web/ActionServlet.java
+++ web04-3/src/web/ActionServlet.java
@@ -12,6 +12,17 @@ public class ActionServlet extends HttpServlet {
 
 	public void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-        System.out.println("actionServlet's  service...");		
+        System.out.println("actionServlet's  service...");	
+        //获得请求资源路径
+        String uri = request.getRequestURI();
+        System.out.println(uri);
+        //分析请求资源路径
+        String action = uri.substring(uri.lastIndexOf("/"),uri.lastIndexOf("."));
+        System.out.println(action);
+        if("/list".equals(action)){
+        	System.out.println("员工列表");
+        }else if("/add".equals(action)){
+        	System.out.println("添加员工");
+        }
 	}
 }
