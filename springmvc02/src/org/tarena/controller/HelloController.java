c1f793b45b57f53c953293173d136dd2f3875659
taizlinger123 <837337164@qq.com>
2020-04-19 16:45:57 +0800

    2020-4-19

--- springmvc02/src/org/tarena/controller/HelloController.java
+++ springmvc02/src/org/tarena/controller/HelloController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller//扫描到Spring容器
+@Controller//ɨ�赽Spring
 public class HelloController {
 	
    @RequestMapping("/hello.do")
    public ModelAndView execute(){
 	   ModelAndView mav = new ModelAndView();
 	   mav.setViewName("hello");
-	   mav.getModel().put("msg", "注解版");
+	   mav.getModel().put("msg", "ע���");
 	   return mav;
    }
 }
