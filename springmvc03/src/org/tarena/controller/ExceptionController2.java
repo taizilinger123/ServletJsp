283a61bc605cd5e89dd5e8f8da2329d6432b0682
taizlinger123 <837337164@qq.com>
2020-04-23 20:39:31 +0800

    2020-4-23

--- springmvc03/src/org/tarena/controller/ExceptionController2.java
+++ springmvc03/src/org/tarena/controller/ExceptionController2.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ExceptionController2 {
 	
-   @RequestMapping("/exception1.do")
+   @RequestMapping("/exception2.do")
    public String ex1(){
 		   String s = null;
 		   s.length();
