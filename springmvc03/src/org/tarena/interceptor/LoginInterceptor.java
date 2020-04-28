20d25debaad37033d9e826bcabb82cbb80addd5f
taizlinger123 <837337164@qq.com>
2020-04-28 21:20:20 +0800

    2020-4-28

--- springmvc03/src/org/tarena/interceptor/LoginInterceptor.java
+++ springmvc03/src/org/tarena/interceptor/LoginInterceptor.java
@@ -2,6 +2,7 @@ package org.tarena.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,11 +24,19 @@ public class LoginInterceptor implements HandlerInterceptor{
 		
 	}
     //controller之前
-	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
 			Object arg2) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("--preHandle--");
-		return true;
+		System.out.println("--preHandle实现登录检查--");
+		HttpSession session = request.getSession();
+		//获取登录成功后放置的用户信息
+		String name = (String)session.getAttribute("username");
+		if(name != null){//登录过
+			return true;//继续执行mvc后续流程
+		}else{//未登录或登录失效
+			response.sendRedirect("tologin.do");
+			return false;//终止mvc后续流程
+		}
+		
 	}
     
 }
