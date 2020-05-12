e0e1879c82aa818478a0c97fc12c2895fcf60ae6
taizlinger123 <837337164@qq.com>
2020-05-12 22:42:49 +0800

    2020-5-12

---
+++ mybatis/src/org/tarena/entity/Emp.java
@@ -0,0 +1,43 @@
+package org.tarena.entity;
+
+import java.io.Serializable;
+
+public class Emp implements Serializable {
+	private Integer id;//属性名与emp表字段一致
+	private String name;
+	private Double salary;
+	private Integer age;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
