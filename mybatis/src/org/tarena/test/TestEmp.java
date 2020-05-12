9e09b75261ba6f74583cd34e15b646ef66a0703f
taizlinger123 <837337164@qq.com>
2020-05-12 22:57:00 +0800

    2020-5-12

--- mybatis/src/org/tarena/test/TestEmp.java
+++ mybatis/src/org/tarena/test/TestEmp.java
@@ -1,13 +1,7 @@
 package org.tarena.test;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
-
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class TestEmp {
 
@@ -16,15 +10,7 @@ public class TestEmp {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-	   SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-	   //加载SqlMapConfig.xml文件
-	   String conf = "SqlMapConfig.xml";
-	   //Reader reader = Resources.getResourceAsReader(conf);
-	   InputStream confStream = TestEmp.class.getClassLoader().getResourceAsStream(conf);
-	   //获取SqlSessionFactory
-	   SqlSessionFactory factory = builder.build(confStream);
-       //获取SqlSession
-	   SqlSession session = factory.openSession();
+       SqlSession session = MyBatisUtil.getSqlSession();
 	   System.out.println("使用session操作");
 	   session.close();//释放session
 	}
