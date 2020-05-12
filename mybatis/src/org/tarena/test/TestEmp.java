7c732761e4d2f79fca28908658e9df8cd3a4e8a9
taizlinger123 <837337164@qq.com>
2020-05-12 23:31:17 +0800

    2020-5-12

--- mybatis/src/org/tarena/test/TestEmp.java
+++ mybatis/src/org/tarena/test/TestEmp.java
@@ -14,11 +14,19 @@ public class TestEmp {
 	 */
 	public static void main(String[] args) throws IOException {
        SqlSession session = MyBatisUtil.getSqlSession();
+       //测试多行查询
        //调用id=findAll的SQL
 //       List<Emp> list = session.selectList("findAll");
-       List<Emp> list = session.selectList("findLikeName","%月%");
-       for(Emp e : list){
-    	   System.out.println(e.getName());
+//       List<Emp> list = session.selectList("findLikeName","%月%");
+//       for(Emp e : list){
+//    	   System.out.println(e.getName());
+//       }
+       //测试单行查询
+       Emp emp = session.selectOne("findById",18);
+       if(emp != null){
+           System.out.println(emp.getName());
+       }else{
+    	   System.out.println("未查到记录");
        }
 	   session.close();//释放session
 	}
